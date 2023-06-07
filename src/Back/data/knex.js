e59339c03209406a06d9662b665ed1aeaa3714fa
hagi_takumi <takumi_hagi@mail.toyota.co.jp>
2023-06-08 06:28:03 +0900

    migrate-latest追加2

--- src/Back/data/knex.js
+++ src/Back/data/knex.js
@@ -6,6 +6,6 @@
 
 const knex = require("knex");
 const knexConfig = require("./knexfile");
-
+console.log(process.env.DATABASE_URL);
 const environment = process.env.DATABASE_URL ? "production" : "development";
 module.exports = knex(knexConfig[environment]);
